e43d5e84fa33db3b9b6b679531d3fdc9fcf8ed3e
michael basweti <baswetima@gmail.com>
2025-07-17 10:52:58 +0300

    get book by title

--- books/src/main/java/com/mbasweti/books/controller/BookController.java
+++ books/src/main/java/com/mbasweti/books/controller/BookController.java
@@ -2,6 +2,7 @@ package com.mbasweti.books.controller;
 
 import com.mbasweti.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -36,6 +37,23 @@ public class BookController {
     @GetMapping("api/books")
     public List<Book> getBooks(){
         return books;
+    }
+
+    @GetMapping("api/books/{title}")
+    public Book getBookByTitle(@PathVariable String title) {
+
+//    1st option
+//        for(Book book: books){
+//            if(book.getTitle().equalsIgnoreCase(title)){
+//                return book;
+//            }
+//        }
+//        return null;
+
+        return books.stream()
+                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElse(null);
     }
 
 }
