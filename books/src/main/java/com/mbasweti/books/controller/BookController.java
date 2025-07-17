a216542531fc04afc8fecf5dfe2501f748d1ca4d
michael basweti <baswetima@gmail.com>
2025-07-17 12:16:04 +0300

    query parameters to filter books

--- books/src/main/java/com/mbasweti/books/controller/BookController.java
+++ books/src/main/java/com/mbasweti/books/controller/BookController.java
@@ -3,6 +3,7 @@ package com.mbasweti.books.controller;
 import com.mbasweti.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -34,21 +35,49 @@ public class BookController {
         return "hello mike";
     }
 
+/*
+    get all books
     @GetMapping("api/books")
     public List<Book> getBooks(){
         return books;
+    }
+*/
+
+    @GetMapping("api/books")
+    public List<Book> getBooks(@RequestParam(required = false) String category){
+        if(category == null){
+            return books;
+        }
+
+/*
+        List<Book> filteredBooks = new ArrayList<>();
+
+        for (Book book : books){
+            if (book.getCategory().equalsIgnoreCase(category)){
+                filteredBooks.add(book);
+            }
+        }
+
+        return filteredBooks;
+*/
+
+        return books.stream()
+                .filter(book -> book.getCategory().equalsIgnoreCase(category))
+                .toList();
     }
 
     @GetMapping("api/books/{title}")
     public Book getBookByTitle(@PathVariable String title) {
 
-//    1st option
-//        for(Book book: books){
-//            if(book.getTitle().equalsIgnoreCase(title)){
-//                return book;
-//            }
-//        }
-//        return null;
+/*
+    1st option
+        for(Book book: books){
+            if(book.getTitle().equalsIgnoreCase(title)){
+                return book;
+            }
+        }
+        return null;
+*/
 
         return books.stream()
                 .filter(book -> book.getTitle().equalsIgnoreCase(title))
